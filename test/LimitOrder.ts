6b214225331a44ff5d3472c391b982de165e5575
Aditya Rout <adi.b20612@gmail.com>
2023-09-15 22:31:59 +0530

    test: Add tests for limit orders

---
+++ test/LimitOrder.ts
@@ -0,0 +1,305 @@
+import { Decimal } from 'decimal.js'
+import { BigNumber, BigNumberish, ContractTransaction, Wallet } from 'ethers'
+import { ethers, waffle } from 'hardhat'
+import { MockTimeUniswapV3Pool } from '../typechain/MockTimeUniswapV3Pool'
+import { TestERC20 } from '../typechain/TestERC20'
+import { SqrtPriceMathTest } from '../typechain/SqrtPriceMathTest'
+
+import { TestUniswapV3Callee } from '../typechain/TestUniswapV3Callee'
+import { expect } from './shared/expect'
+import { poolFixture } from './shared/fixtures'
+import { formatPrice, formatTokenAmount } from './shared/format'
+import {
+  createPoolFunctions,
+  encodePriceSqrt,
+  expandTo18Decimals,
+  FeeAmount,
+  getMaxLiquidityPerTick,
+  getMaxTick,
+  getMinTick,
+  MAX_SQRT_RATIO,
+  MaxUint128,
+  MIN_SQRT_RATIO,
+  TICK_SPACINGS,
+} from './shared/utilities'
+
+Decimal.config({ toExpNeg: -500, toExpPos: 500 })
+
+const createFixtureLoader = waffle.createFixtureLoader
+const { constants } = ethers
+
+interface BaseSwapTestCase {
+  zeroForOne: boolean
+  sqrtPriceLimit?: BigNumber
+}
+interface SwapExact0For1TestCase extends BaseSwapTestCase {
+  zeroForOne: true
+  exactOut: false
+  amount0: BigNumberish
+  sqrtPriceLimit?: BigNumber
+}
+interface SwapExact1For0TestCase extends BaseSwapTestCase {
+  zeroForOne: false
+  exactOut: false
+  amount1: BigNumberish
+  sqrtPriceLimit?: BigNumber
+}
+interface Swap0ForExact1TestCase extends BaseSwapTestCase {
+  zeroForOne: true
+  exactOut: true
+  amount1: BigNumberish
+  sqrtPriceLimit?: BigNumber
+}
+interface Swap1ForExact0TestCase extends BaseSwapTestCase {
+  zeroForOne: false
+  exactOut: true
+  amount0: BigNumberish
+  sqrtPriceLimit?: BigNumber
+}
+interface SwapToHigherPrice extends BaseSwapTestCase {
+  zeroForOne: false
+  sqrtPriceLimit: BigNumber
+}
+interface SwapToLowerPrice extends BaseSwapTestCase {
+  zeroForOne: true
+  sqrtPriceLimit: BigNumber
+}
+type SwapTestCase =
+  | SwapExact0For1TestCase
+  | Swap0ForExact1TestCase
+  | SwapExact1For0TestCase
+  | Swap1ForExact0TestCase
+  | SwapToHigherPrice
+  | SwapToLowerPrice
+
+function swapCaseToDescription(testCase: SwapTestCase): string {
+  const priceClause = testCase?.sqrtPriceLimit ? ` to price ${formatPrice(testCase.sqrtPriceLimit)}` : ''
+  if ('exactOut' in testCase) {
+    if (testCase.exactOut) {
+      if (testCase.zeroForOne) {
+        return `swap token0 for exactly ${formatTokenAmount(testCase.amount1)} token1${priceClause}`
+      } else {
+        return `swap token1 for exactly ${formatTokenAmount(testCase.amount0)} token0${priceClause}`
+      }
+    } else {
+      if (testCase.zeroForOne) {
+        return `swap exactly ${formatTokenAmount(testCase.amount0)} token0 for token1${priceClause}`
+      } else {
+        return `swap exactly ${formatTokenAmount(testCase.amount1)} token1 for token0${priceClause}`
+      }
+    }
+  } else {
+    if (testCase.zeroForOne) {
+      return `swap token0 for token1${priceClause}`
+    } else {
+      return `swap token1 for token0${priceClause}`
+    }
+  }
+}
+
+type PoolFunctions = ReturnType<typeof createPoolFunctions>
+
+// can't use address zero because the ERC20 token does not allow it
+const SWAP_RECIPIENT_ADDRESS = constants.AddressZero.slice(0, -1) + '1'
+const POSITION_PROCEEDS_OUTPUT_ADDRESS = constants.AddressZero.slice(0, -1) + '2'
+
+async function executeSwap(
+  pool: MockTimeUniswapV3Pool,
+  testCase: SwapTestCase,
+  poolFunctions: PoolFunctions
+): Promise<ContractTransaction> {
+  let swap: ContractTransaction
+  if ('exactOut' in testCase) {
+    if (testCase.exactOut) {
+      if (testCase.zeroForOne) {
+        swap = await poolFunctions.swap0ForExact1(testCase.amount1, SWAP_RECIPIENT_ADDRESS, testCase.sqrtPriceLimit)
+      } else {
+        swap = await poolFunctions.swap1ForExact0(testCase.amount0, SWAP_RECIPIENT_ADDRESS, testCase.sqrtPriceLimit)
+      }
+    } else {
+      if (testCase.zeroForOne) {
+        swap = await poolFunctions.swapExact0For1(testCase.amount0, SWAP_RECIPIENT_ADDRESS, testCase.sqrtPriceLimit)
+      } else {
+        swap = await poolFunctions.swapExact1For0(testCase.amount1, SWAP_RECIPIENT_ADDRESS, testCase.sqrtPriceLimit)
+      }
+    }
+  } else {
+    if (testCase.zeroForOne) {
+      swap = await poolFunctions.swapToLowerPrice(testCase.sqrtPriceLimit, SWAP_RECIPIENT_ADDRESS)
+    } else {
+      swap = await poolFunctions.swapToHigherPrice(testCase.sqrtPriceLimit, SWAP_RECIPIENT_ADDRESS)
+    }
+  }
+  return swap
+}
+
+const DEFAULT_POOL_SWAP_TEST: SwapTestCase = 
+  {
+    zeroForOne: false,
+    exactOut: false,
+    amount1: expandTo18Decimals(3),
+  };
+
+
+interface Position {
+  tickLower: number
+  tickUpper: number
+  liquidity: BigNumberish
+}
+
+interface PoolTestCase {
+  description: string
+  feeAmount: number
+  tickSpacing: number
+  startingPrice: BigNumber
+  positions: Position[]
+  swapTests?: SwapTestCase[]
+}
+
+const poolCase: PoolTestCase = 
+  {
+    description: 'low fee, 1:1 price, 2e18 max range liquidity',
+    feeAmount: FeeAmount.LOW,
+    tickSpacing: TICK_SPACINGS[FeeAmount.LOW],
+    startingPrice: encodePriceSqrt(1, 1),
+    positions: [
+      {
+        tickLower: getMinTick(TICK_SPACINGS[FeeAmount.LOW]),
+        tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.LOW]),
+        liquidity: expandTo18Decimals(2),
+      },
+    ],
+  };
+
+describe('Limit Order tests', () => {
+  let wallet: Wallet, other: Wallet
+
+  let loadFixture: ReturnType<typeof createFixtureLoader>
+  let sqrtPriceMath: SqrtPriceMathTest
+  before('create fixture loader', async () => {
+    ;[wallet, other] = await (ethers as any).getSigners()
+    const sqrtPriceMathTestFactory = await ethers.getContractFactory('SqrtPriceMathTest')
+    sqrtPriceMath = (await sqrtPriceMathTestFactory.deploy()) as SqrtPriceMathTest
+    loadFixture = createFixtureLoader([wallet])
+  })
+
+    describe(poolCase.description, () => {
+      const poolCaseFixture = async () => {
+        const { createPool, token0, token1, swapTargetCallee: swapTarget } = await poolFixture(
+          [wallet],
+          waffle.provider
+        )
+        const pool = await createPool(poolCase.feeAmount, poolCase.tickSpacing)
+        const poolFunctions = createPoolFunctions({ swapTarget, token0, token1, pool })
+        await pool.initialize(poolCase.startingPrice)
+        // mint all positions
+        for (const position of poolCase.positions) {
+          await poolFunctions.mint(wallet.address, position.tickLower, position.tickUpper, position.liquidity)
+        }
+
+        const [poolBalance0, poolBalance1] = await Promise.all([
+          token0.balanceOf(pool.address),
+          token1.balanceOf(pool.address),
+        ])
+
+        return { token0, token1, pool, poolFunctions, poolBalance0, poolBalance1, swapTarget }
+      }
+
+      let token0: TestERC20
+      let token1: TestERC20
+
+      let poolBalance0: BigNumber
+      let poolBalance1: BigNumber
+
+      let pool: MockTimeUniswapV3Pool
+      let swapTarget: TestUniswapV3Callee
+      let poolFunctions: PoolFunctions
+
+      
+
+      beforeEach('load fixture', async () => {
+        ;({ token0, token1, pool, poolFunctions, poolBalance0, poolBalance1, swapTarget } = await loadFixture(
+          poolCaseFixture
+        ))
+      })
+      it("create limit order", async () => {
+        const slot0 = await pool.slot0()
+        const [
+          poolBalance0After,
+          poolBalance1After,
+          slot0After,
+          liquidityAfter,
+          feeGrowthGlobal0X128,
+          feeGrowthGlobal1X128,
+        ] = await Promise.all([
+          token0.balanceOf(pool.address),
+          token1.balanceOf(pool.address),
+          pool.slot0(),
+          pool.liquidity(),
+          pool.feeGrowthGlobal0X128(),
+          pool.feeGrowthGlobal1X128(),
+        ])
+
+        const bl0 = await token0.balanceOf(wallet.address);
+        await token0.approve(pool.address, bl0);
+        const bl1 = await token1.balanceOf(wallet.address);
+        await token1.approve(pool.address, bl1);
+        const tx =  pool.connect(wallet).createLimitOrder(wallet.address, slot0.tick, 100000000);
+        await expect(tx).to.emit(pool, "Mint");
+      })
+      it("cancel limit order", async () => {
+        const slot0 = await pool.slot0()
+        const [
+          poolBalance0After,
+          poolBalance1After,
+          slot0After,
+          liquidityAfter,
+          feeGrowthGlobal0X128,
+          feeGrowthGlobal1X128,
+        ] = await Promise.all([
+          token0.balanceOf(pool.address),
+          token1.balanceOf(pool.address),
+          pool.slot0(),
+          pool.liquidity(),
+          pool.feeGrowthGlobal0X128(),
+          pool.feeGrowthGlobal1X128(),
+        ])
+
+        const bl0 = await token0.balanceOf(wallet.address);
+        await token0.approve(pool.address, bl0);
+        const bl1 = await token1.balanceOf(wallet.address);
+        await token1.approve(pool.address, bl1);
+        await pool.createLimitOrder(wallet.address, slot0.tick, 100000000);
+        const tx = pool.connect(wallet).cancelLimitOrder(wallet.address, slot0.tick);
+        await expect(tx).to.emit(token0, "Transfer");
+      })
+      it("collect limit order", async () => {
+        const slot0 = await pool.slot0()
+        const [
+          poolBalance0After,
+          poolBalance1After,
+          slot0After,
+          liquidityAfter,
+          feeGrowthGlobal0X128,
+          feeGrowthGlobal1X128,
+        ] = await Promise.all([
+          token0.balanceOf(pool.address),
+          token1.balanceOf(pool.address),
+          pool.slot0(),
+          pool.liquidity(),
+          pool.feeGrowthGlobal0X128(),
+          pool.feeGrowthGlobal1X128(),
+        ])
+
+        const bl0 = await token0.balanceOf(wallet.address);
+        await token0.approve(pool.address, bl0);
+        const bl1 = await token1.balanceOf(wallet.address);
+        await token1.approve(pool.address, bl1);
+        await pool.createLimitOrder(wallet.address, slot0.tick, 100000000);
+        await executeSwap(pool, DEFAULT_POOL_SWAP_TEST, poolFunctions);
+        const tx = pool.connect(wallet).collectLimitOrder(wallet.address, slot0.tick);
+
+        await expect(tx).to.emit(token1, "Transfer");
+      })
+    })
+})
